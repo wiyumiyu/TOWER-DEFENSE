b1ed294d6ba7d8a44ea3e5b797d2aab1720f8c59
WIyumiyu <mariana.torres.valverde0@gmail.com>
2024-10-31 14:46:13 -0600

    ERTY

---
+++ src/main/java/com/wiyumiyu/ed_towerdefense_marianatorres/ED_TOWERDEFENSE_MARIANATORRES.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.wiyumiyu.ed_towerdefense_marianatorres;
+
+/**
+ *
+ * @author mariana
+ */
+public class ED_TOWERDEFENSE_MARIANATORRES {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
