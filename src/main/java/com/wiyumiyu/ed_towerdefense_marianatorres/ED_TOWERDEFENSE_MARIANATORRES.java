fcae005580e76843b624158613d55eb26120b24b
WIyumiyu <mariana.torres.valverde0@gmail.com>
2024-10-31 14:48:49 -0600

    Prueba

--- src/main/java/com/wiyumiyu/ed_towerdefense_marianatorres/ED_TOWERDEFENSE_MARIANATORRES.java
+++ src/main/java/com/wiyumiyu/ed_towerdefense_marianatorres/ED_TOWERDEFENSE_MARIANATORRES.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.wiyumiyu.ed_towerdefense_marianatorres;
 
-/**
- *
- * @author mariana
- */
 public class ED_TOWERDEFENSE_MARIANATORRES {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Probando repo");
     }
 }
